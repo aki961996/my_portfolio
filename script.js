eb636827867f4d033a134c8686a0890d36917c8e
akhilesh <akhiles@veuz.in>
2025-04-24 13:56:08 +0530

    p

---
+++ script.js
@@ -0,0 +1,70 @@
+
+// mail msg
+// Initialize EmailJS with your public key (user ID)
+
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'service_i37nw48';
+   const templateID = 'template_gnfcazh';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+    //   alert('Sent!');
+    showNotification();
+    document.getElementById('form').reset();
+
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
+
+
+
+function showNotification() {
+    const note = document.getElementById('notification');
+    note.style.display = 'block';
+
+    setTimeout(() => {
+        note.style.display = 'none';
+    }, 3000);
+}
+
+
+
+
+const scrollTopBtn = document.getElementById('scrollTopBtn');
+window.addEventListener('scroll', () => {
+    scrollTopBtn.style.display = window.scrollY > 300 ? 'block' : 'none';
+});
+
+scrollTopBtn.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+// Scroll animation
+const animatedElements = document.querySelectorAll('section');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('in-view');
+        }
+    });
+}, {
+    threshold: 0.1
+});
+
+animatedElements.forEach(el => {
+    el.classList.add('animate');
+    observer.observe(el);
+});
+
+
